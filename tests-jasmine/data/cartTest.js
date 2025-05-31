5e9abefabee649a3ceb3fe62d4a88caeb86c5c68
Zerwolf729 <samueljuliank29@gmail.com>
2025-05-31 16:09:10 +0800

    Create automated tests for orderSummary

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart, cart, loadFromStorage} from '../../data/cart.js'
+import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
 describe('Test suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
